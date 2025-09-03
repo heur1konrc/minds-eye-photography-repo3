c3399e2636176dff389f7988404c3cef5da8ec96
heur1konrc <Rick@RickCorey.com>
2025-09-03 01:12:43 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,7 +15,6 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: undefined,
